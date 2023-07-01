b003c2f4a6b0a121a0ec08706d72f0bc2ba9eb72
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-07-01 13:43:33 -0300

    Build: arquivos base de configuração

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import MyNavbar from './components/navbar/navbar'
+import MyCard from './components/card/card'
+import MyForm from './components/form/form'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <MyNavbar />
+      <MyCard />
+      <MyForm />
+    </>
+  )
+}
+
+export default App
