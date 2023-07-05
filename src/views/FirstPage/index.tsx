3da832bab28dfb735e90e35c21d0a8d6243d8ec5
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2023-07-05 20:11:22 -0300

    fix: Cria FirstPage

---
+++ src/views/FirstPage/index.tsx
@@ -0,0 +1,15 @@
+import MyNavbar from "../../components/navbar/navbar"
+import MyCard from '../../components/card/card'
+import MyForm from '../../components/form/form'
+import '../../App.css'
+
+
+export default function FirstPage() {
+    return (
+        <>
+            <MyNavbar />
+            <MyCard />
+            <MyForm />
+        </>
+    )
+}
