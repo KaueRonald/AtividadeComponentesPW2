e72ac2112477bce0ed2a186d6cb255a1eff2e0b9
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-07-05 22:21:13 -0300

    refactor: refatorando código com css ja do react-bootstrap

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,6 +6,7 @@ function MyNavbar() {
   return (
     <Navbar expand="lg" className="navbar">
       <Navbar.Brand href="#" className="navbar-brand">Meu Site</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarCollapse" className="navbar-toggler" />
       <Navbar.Collapse id="navbarCollapse">
         <Nav className="ml-auto">
           <Nav.Link href="/" className="nav-link">Início</Nav.Link>
