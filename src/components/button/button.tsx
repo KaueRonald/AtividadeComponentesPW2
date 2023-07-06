9d9a7c05e3a22b438872f28706fa08682c9d6161
Luis Felipe M <luis.felippe@academico.ifpb.edu.br>
2023-07-05 21:37:38 -0300

    Criando componente Button

---
+++ src/components/button/button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function button(){
+    return(
+      <>
+            <Button href='https://react-bootstrap.netlify.app/' target='blank' variant="warning">Site Oficial</Button>
+    </>
+    );
+
+
+}
+
+export default button;
